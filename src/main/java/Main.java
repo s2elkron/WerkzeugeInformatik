f3bdc584f9cfe84cb9e38de54f9e998da66720e7
Elias <s2elkron@uni-trier.de>
2022-05-15 16:33:59 +0200

    modified:   src/main/java/Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,9 @@ public class Main {
         Car car1 = new Car("red", "BMW", 5);
         Car car2 = new Car("blue", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
+        Car car4 = new Car("black", "Mercedes-Benz", 5);
 
         factory.buildCar(car1);
-        factory.buildCar(car2);
-	  factory.buildCar(car2);	
+        factory.buildCar(car2);	
     }
 }
